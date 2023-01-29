d61d9b485f73b5732f09e54bf6bff4338649ae6d
Lynix <lynix@lynix.ca>
2023-01-29 14:26:16 +0000

    Implement Leaderboard V2

--- components/leaderboards/TribeLeaderboard.tsx
+++ components/leaderboards/TribeLeaderboard.tsx
@@ -13,11 +13,11 @@ const TribeLeaderboard = () => {
 
     useEffect(() => {
         const timerId = setTimeout(() => {
-          setdebounceSearch(search);
+            setdebounceSearch(search);
         }, 250);
-        
+
         return () => {
-          clearTimeout(timerId);
+            clearTimeout(timerId);
         };
     }, [search]);
 
@@ -25,6 +25,9 @@ const TribeLeaderboard = () => {
     const [clusterDropdown, setClusterDropdown] = useState(false);
     const handleClusterDropdown = () => setClusterDropdown(!clusterDropdown);
 
+    /* Filter Dropdown */
+    const [filterDropdown, setFilterDropdown] = useState(false);
+    const handleFilterDropdown = () => setFilterDropdown(!filterDropdown);
 
     /* Cluster Filter */
     const [clusterUrl, setClusterUrl] = useState(`/api/ark/4man/tribe_rankings`);
@@ -34,18 +37,18 @@ const TribeLeaderboard = () => {
 
     useEffect(() => {
         const timerId = setTimeout(() => {
-          if(clusterFilter == 1) {
-            setClusterUrl(`/api/ark/6man/tribe_rankings`)
-            setClusterDropdown(false)
-          } else {
-            setClusterUrl(`/api/ark/4man/tribe_rankings`)
-            setClusterDropdown(false)
-          }
-          console.log(clusterUrl)
+            if (clusterFilter == 1) {
+                setClusterUrl(`/api/ark/6man/tribe_rankings`)
+                setClusterDropdown(false)
+            } else {
+                setClusterUrl(`/api/ark/4man/tribe_rankings`)
+                setClusterDropdown(false)
+            }
+            console.log(clusterUrl)
         }, 250);
-        
+
         return () => {
-          clearTimeout(timerId);
+            clearTimeout(timerId);
         };
     }, [clusterFilter]);
 
@@ -53,7 +56,7 @@ const TribeLeaderboard = () => {
     const { data, error }: any = useSWR(`${clusterUrl}?search=${debounceSearch}`, fetcher)
 
     return (<>
-        <h2 className="font-extrabold text-mesa-orange uppercase text-xl mt-5">
+        <h2 className="font-extrabold text-gray-300 uppercase text-xl mt-5">
             Tribe Leaderboards
         </h2>
         {/* Filters */}
@@ -61,19 +64,33 @@ const TribeLeaderboard = () => {
             <input
                 value={search}
                 onChange={handleOnChange}
-                placeholder="Search for Tribes" name="tribe_search" id="tribe_search" className="px-3 py-2 text-gray-300 bg-mesa-gray w-1/2 border-gray-500 border" />
-            
+                placeholder="Search for Tribes" name="tribe_search" id="tribe_search" className="px-3 py-2 text-gray-300 bg-bgray-overlay w-1/2 border-gray-700 border rounded-full" />
+
             <div className="relative">
-                <button onClick={handleClusterDropdown} className="px-3 py-2 text-white bg-mesa-orange font-bold">
-                {clusterDropdown 
-                    ? <>Cluster: {clusterFilter == 0 && "4"} {clusterFilter == 1 && "6"} Man <i className="ml-1 fa-solid fa-angle-up"></i></>
-                    : <>Cluster: {clusterFilter == 0 && "4"} {clusterFilter == 1 && "6"} Man <i className="ml-1 fa-solid fa-angle-down"></i></>
-                }
+                <button onClick={handleClusterDropdown} className="px-3 py-2 text-white bg-bgray-overlay font-bold rounded-full">
+                    {clusterDropdown
+                        ? <>{clusterFilter == 0 && "4"} {clusterFilter == 1 && "6"} Man <i className="ml-1 fa-solid fa-angle-up"></i></>
+                        : <>{clusterFilter == 0 && "4"} {clusterFilter == 1 && "6"} Man <i className="ml-1 fa-solid fa-angle-down"></i></>
+                    }
                 </button>
-                <div className={clusterDropdown ? 'absolute z-50 mt-3 w-48 shadow-lg origin-top-left left-0' : 'hidden z-50 mt-3 w-48 shadow-lg origin-top-left left-0'}>
-                    <div className="ring-1 ring-black ring-opacity-5 py-1 bg-mesa-dropdown">
-                    <button onClick={handlePrimaryCluster} className="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-100 hover:bg-mesa-gray focus:outline-none focus:bg-mesa-gray transition duration-150 ease-in-out">4 Man Cluster</button>
-                    <button onClick={handleSecondaryCluster} className="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-100 hover:bg-mesa-gray focus:outline-none focus:bg-mesa-gray transition duration-150 ease-in-out">6 Man Cluster</button>
+                <div className={clusterDropdown ? 'absolute z-50 mt-3 w-48 shadow-lg origin-top-left left-0 rounded-2xl' : 'hidden z-50 mt-3 w-48 shadow-lg origin-top-left left-0 rounded-2xl'}>
+                    <div className="ring-1 ring-black ring-opacity-5 py-1 bg-mesa-dropdown rounded-2xl">
+                        <button onClick={handlePrimaryCluster} className="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-100 hover:bg-mesa-gray focus:outline-none focus:bg-mesa-gray transition duration-150 ease-in-out">4 Man Cluster</button>
+                        <button onClick={handleSecondaryCluster} className="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-100 hover:bg-mesa-gray focus:outline-none focus:bg-mesa-gray transition duration-150 ease-in-out">6 Man Cluster</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="relative">
+                <button onClick={handleFilterDropdown} className="px-3 py-2 text-white bg-bgray-overlay font-bold rounded-full">
+                    Sort By : ??? <i className="ml-1 fa-solid fa-angle-down" />
+                </button>
+                <div className={filterDropdown ? 'absolute z-50 mt-3 w-48 shadow-lg origin-top-left left-0' : 'hidden z-50 mt-3 w-48 shadow-lg origin-top-left left-0'}>
+                    <div className="ring-1 ring-black ring-opacity-5 py-1 bg-mesa-dropdown rounded-2xl">
+                        <button className="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-100 hover:bg-mesa-gray focus:outline-none focus:bg-mesa-gray transition duration-150 ease-in-out">Kills</button>
+                        <button className="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-100 hover:bg-mesa-gray focus:outline-none focus:bg-mesa-gray transition duration-150 ease-in-out">Deaths</button>
+                        <button className="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-100 hover:bg-mesa-gray focus:outline-none focus:bg-mesa-gray transition duration-150 ease-in-out">Tame Kills</button>
+                        <button className="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-100 hover:bg-mesa-gray focus:outline-none focus:bg-mesa-gray transition duration-150 ease-in-out">Time Played</button>
                     </div>
                 </div>
             </div>
@@ -91,7 +108,7 @@ const TribeLeaderboard = () => {
                                     v-if="$page.props.stats != null"
                                 >
                                     <tbody>
-                                        <tr className="focus:outline-none h-12 border-t border-b-[1px] border-mesa-black bg-mesa-orange">
+                                        <tr className="focus:outline-none h-12 border-t border-b-[2px] border-bgray-bg bg-bgray-overlay">
                                             <td className={search ? "hidden pl-5" : "pl-5"}>
                                                 <div className="flex items-center">
                                                     <p className="text-base leading-none text-white font-bold uppercase">
@@ -151,7 +168,7 @@ const TribeLeaderboard = () => {
                                         </tr>
                                         {data?.ranking_data?.map((tribe: any, rank: any) => {
                                             return (
-                                                <tr key={tribe?.TribeName} className="focus:outline-none h-12 border-t border-b-[1px] border-mesa-black bg-mesa-gray">
+                                                <tr key={tribe?.TribeName} className="focus:outline-none h-12 border-t border-b-[2px] border-bgray-bg bg-bgray-secondary">
                                                     <td className={search ? "hidden" : "pl-5"}>
                                                         <div className="flex items-center">
                                                             <p className="text-base leading-none text-white font-bold">
@@ -250,6 +267,17 @@ const TribeLeaderboard = () => {
                     </div>
                 </div>
             )}
+            <p className="text-gray-300">Page <strong>{data?.pagination?.current_page + 1}</strong> of <strong>{data?.pagination?.total_pages + 1}</strong></p>
+            <div className="flex space-x-2 mt-3">
+                {data?.pagination?.prev ?
+                    <a href={(data?.pagination?.prev).replace("https://bloody.gg/api/hub/rankings", "https://bloody.gg/hub/rankings")} className="inline-flex items-center px-3 py-1 font-bold leading-6 text-md shadow rounded-full text-gray-100  bg-bgray-overlay transition ease-in-out duration-150">  <i className="fa-solid fa-arrow-left m-1 mr-2 my-auto"></i> Previous</a>
+                    : <></>
+                }
+                {data?.pagination?.next ?
+                    <a href={(data?.pagination?.next).replace("https://bloody.gg/api/hub/rankings", "https://bloody.gg/hub/rankings")} className="inline-flex items-center px-3 py-1 font-bold leading-6 text-md shadow rounded-full text-gray-100 bg-bgray-overlay transition ease-in-out duration-150">  Next <i className="fa-solid fa-arrow-right m-1 ml-1 my-auto"></i></a>
+                    : <></>
+                }
+            </div>
         </div>
     </>)
 }
