6356fa2d065cb3c9798be208de10fc39f94534a3
Lynix <81735347+lunarlynix@users.noreply.github.com>
2023-02-05 16:16:02 -0400

    Fix Tribe Pagination

--- pages/api/ark/4man/tribe_rankings.ts
+++ pages/api/ark/4man/tribe_rankings.ts
@@ -75,11 +75,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   var prev_page = null;
 
   if(current_page < 69) {
-    next_page = `https://mesa-ark.com/api/ark/tribe_rankings?page=${parseInt(current_page as CurrentPage) + 1}&search=${search}`;
+    next_page = `https://mesa-ark.com/api/ark/tribe_rankings?page=${current_page}&search=${search}`;
   }
 
   if(current_page > 0) {
-    prev_page = `https://mesa-ark.com/api/ark/tribe_rankings?page=${parseInt(current_page as CurrentPage) - 1}&search=${search}`;
+    prev_page = `https://mesa-ark.com/api/ark/tribe_rankings?page=${current_page}&search=${search}`;
   }
 
   /* Return All Required Data */
